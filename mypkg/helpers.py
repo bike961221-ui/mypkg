d5022f5ce1590f8c409c2500254126ed58e159bc
재석 김 <bike961221@gmail.com>
2026-03-03 19:09:09 +0900

    Fix missing add fucntion

--- mypkg/helpers.py
+++ mypkg/helpers.py
@@ -4,3 +4,13 @@ def calculate_stats(data):
         "min": min(data),
         "max": max(data),
     }
+def add(a, b):
+    return a + b
+
+
+def calculate_stats(data):
+    return {
+        "mean": sum(data) / len(data),
+        "min": min(data),
+        "max": max(data),
+    }
